b4c545819a937cda6fc5ceec58d669c1a835b701
Zander Erasmus <zerasmus@corvusenergy.com>
2021-10-15 04:01:26 +0000

    Use protocol to allow nested parsing

--- lib/xunit_structures.ex
+++ lib/xunit_structures.ex
@@ -1,48 +1,89 @@
+defprotocol XUnitFormatter.XUnitXML do
+  @fallback_to_any true
+  def element_name(data)
+  def attributes(data)
+  def content(data)
+  def child_elements(data)
+  def xunit_xml(data)
+end
+
+defimpl XUnitFormatter.XUnitXML, for: Any do
+  def element_name(data), do: data.__struct__
+  def attributes(data), do: XUnitFormatter.Modifiers.get_attributes(data)
+  def content(_data), do: []
+  def child_elements(_), do: []
+  def xunit_xml(data), do: {XUnitFormatter.XUnitXML.element_name(data), XUnitFormatter.XUnitXML.attributes(data), XUnitFormatter.XUnitXML.content(data)}
+end
+
+defmodule XUnitFormatter.Modifiers do
+  def map_key_to_xml_key({k, v}) when is_atom(k), do: map_key_to_xml_key({to_string(k), v})
+  def map_key_to_xml_key({k, v}) when is_binary(k), do: {String.replace(k, "_", "-"), v}
+  def attribute_to_xml({k, v}) when is_struct(v), do: {k, XUnitFormatter.XUnitXML.xunit_xml(v)}
+  def attribute_to_xml(attr), do: attr
+  def get_attributes(data), do: data |> Map.from_struct() |> Map.drop(XUnitFormatter.XUnitXML.child_elements(data)) |> Enum.reject(&is_nil/1) |> Enum.map(&attribute_to_xml/1) |> Enum.map(&map_key_to_xml_key/1) |> Enum.into(%{})
+end
+
 defmodule XUnitFormatter.Document do
   import XmlBuilder
   defstruct assemblies: []
 
-  def to_xml(%__MODULE__{assemblies: assemblies}) do
-    document(:assemblies, Enum.map(assemblies, &XUnitFormatter.Assembly.to_xml/1))
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate child_elements(data), to: XUnitFormatter.XUnitXML.Any
+
+    def element_name(_data), do: :assemblies
+    def content(data), do: Enum.map(data.assemblies, &XUnitFormatter.XUnitXML.xunit_xml/1)
+    def xunit_xml(data), do: document(element_name(data), content(data)) |> XmlBuilder.generate()
   end
 end
 
 defmodule XUnitFormatter.RunDate do
-  @enforce_keys [:year, :month, :day]
   defstruct [:year, :month, :day]
 
-  def to_xml(%__MODULE__{year: yyyy, month: mm, day: dd}) do
-    now = DateTime.utc_now()
-    yyyy = (yyyy || now.year) |> Integer.to_string() |> String.pad_leading(4, "0")
-    mm = (mm || now.month) |> Integer.to_string() |> String.pad_leading(2, "0")
-    dd = (dd || now.day) |> Integer.to_string() |> String.pad_leading(2, "0")
-    "#{yyyy}-#{mm}-#{dd}"
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate element_name(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate content(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate child_elements(data), to: XUnitFormatter.XUnitXML.Any
+
+    def xunit_xml(data) do
+      now = DateTime.utc_now()
+      yyyy = (data.year || now.year) |> Integer.to_string() |> String.pad_leading(4, "0")
+      mm = (data.month || now.month) |> Integer.to_string() |> String.pad_leading(2, "0")
+      dd = (data.day || now.day) |> Integer.to_string() |> String.pad_leading(2, "0")
+      "#{yyyy}-#{mm}-#{dd}"
+      end
   end
 end
 
 defmodule XUnitFormatter.RunTime do
-  @enforce_keys [:hour, :minute, :second]
   defstruct [:hour, :minute, :second]
 
-  def to_xml(%__MODULE__{hour: hh, minute: mm, second: ss}) do
-    now = DateTime.utc_now()
-    hh = (hh || now.hour) |> Integer.to_string() |> String.pad_leading(2, "0")
-    mm = (mm || now.minute) |> Integer.to_string() |> String.pad_leading(2, "0")
-    ss = (ss || now.second) |> Integer.to_string() |> String.pad_leading(2, "0")
-    "#{hh}:#{mm}:#{ss}"
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate element_name(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate content(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate child_elements(data), to: XUnitFormatter.XUnitXML.Any
+
+    def xunit_xml(data) do
+      now = DateTime.utc_now()
+      hh = (data.hour || now.hour) |> Integer.to_string() |> String.pad_leading(2, "0")
+      mm = (data.minute|| now.minute) |> Integer.to_string() |> String.pad_leading(2, "0")
+      ss = (data.second || now.second) |> Integer.to_string() |> String.pad_leading(2, "0")
+      "#{hh}:#{mm}:#{ss}"
+    end
   end
 end
 
 defmodule XUnitFormatter.Assembly do
   import XmlBuilder
-  @child_elements [:errors, :collections]
-  @enforce_keys [:name, :run_date, :run_time, :total, :collections]
+  @enforce_keys [:name, :total, :collections]
   defstruct name: nil,
             config_file: nil,
             test_framework: :ex_unit,
             environment: nil,
-            run_date: nil,
-            run_time: nil,
+            run_date: %XUnitFormatter.RunDate{},
+            run_time: %XUnitFormatter.RunTime{},
             time: nil,
             total: nil,
             passed: 0,
@@ -51,27 +92,29 @@ defmodule XUnitFormatter.Assembly do
             errors: [],
             collections: []
 
-  defp map_key_to_xml_key({k, v}) when is_atom(k), do: map_key_to_xml_key({to_string(k), v})
-  defp map_key_to_xml_key({k, v}) when is_binary(k), do: {String.replace(k, "_", "-"), v}
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate xunit_xml(data), to: XUnitFormatter.XUnitXML.Any
 
-  def to_xml(assembly = %__MODULE__{}) do
-    errors = if is_list(assembly.errors) and length(assembly.errors) > 1 do
-      [element(:errors, Enum.map(assembly.errors, &XUnitFormatter.Error.to_xml/1))]
-    else
-      []
+    def element_name(_), do: :assembly
+    def child_elements(_), do: [:errors, :collections]
+    def content(assembly) do
+      errors = if is_list(assembly.errors) and length(assembly.errors) > 1 do
+        [element(:errors, Enum.map(assembly.errors, &XUnitFormatter.XUnitXML.xunit_xml/1))]
+      else
+        []
+      end
+      collections = if is_list(assembly.collections) do
+        Enum.map(assembly.collections, &XUnitFormatter.XUnitXML.xunit_xml/1)
+      else
+        []
+      end
+      errors ++ collections
     end
-    collections = if is_list(assembly.collections) do
-      Enum.map(assembly.collections, &XUnitFormatter.Collection.to_xml/1)
-    else
-      []
-    end
-    attrs = assembly |> Map.from_struct() |> Map.drop(@child_elements) |> Enum.reject(&is_nil/1) |> Enum.map(&map_key_to_xml_key/1) |> Enum.into(%{})
-    {:assembly, attrs, errors ++ collections}
   end
 end
 
 defmodule XUnitFormatter.Collection do
-  @child_elements [:tests]
   @enforce_keys [:name]
   defstruct name: nil,
             time: nil,
@@ -80,41 +123,43 @@ defmodule XUnitFormatter.Collection do
             failed: 0,
             skipped: 0,
             tests: []
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate xunit_xml(data), to: XUnitFormatter.XUnitXML.Any
 
-  defp map_key_to_xml_key({k, v}) when is_atom(k), do: map_key_to_xml_key({to_string(k), v})
-  defp map_key_to_xml_key({k, v}) when is_binary(k), do: {String.replace(k, "_", "-"), v}
-
-  def to_xml(collection = %__MODULE__{}) do
-    tests = if is_list(collection.tests) do
-      Enum.map(collection.tests, &XUnitFormatter.Test.to_xml/1)
-    else
-      []
+    def element_name(_), do: :collection
+    def content(collection) do
+      if is_list(collection.tests) do
+        Enum.map(collection.tests, &XUnitFormatter.XUnitXML.xunit_xml/1)
+      else
+        []
+      end
     end
-
-    attrs = collection |> Map.from_struct() |> Map.drop(@child_elements) |> Enum.reject(&is_nil/1) |> Enum.map(&map_key_to_xml_key/1) |> Enum.into(%{})
-    {:collection, attrs, tests}
+    def child_elements(_), do: [:tests]
   end
 end
 
 defmodule XUnitFormatter.Failure do
-  import XmlBuilder
-  @child_elements [:message, :stack_trace]
   defstruct exception_type: nil,
             message: nil,
             stack_trace: nil
 
-  defp map_key_to_xml_key({k, v}) when is_atom(k), do: map_key_to_xml_key({to_string(k), v})
-  defp map_key_to_xml_key({k, v}) when is_binary(k), do: {String.replace(k, "_", "-"), v}
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate xunit_xml(data), to: XUnitFormatter.XUnitXML.Any
 
-  def to_xml(failure = %__MODULE__{}) do
-    attrs = failure |> Map.from_struct() |> Map.drop(@child_elements) |> Enum.reject(&is_nil/1) |> Enum.map(&map_key_to_xml_key/1) |> Enum.into(%{})
-    element(:failure, attrs, [])
+    def element_name(_), do: :failure
+    def content(data) do
+      [
+        message: {:cdata, data.message},
+        "stack-trace": {:cdata, data.stack_trace}
+      ]
+    end
+    def child_elements(_), do: [:message, :stack_trace]
   end
 end
 
 defmodule XUnitFormatter.Error do
-  import XmlBuilder
-  @child_elements [:failure]
   @enforce_keys [:name]
   defstruct name: nil,
             type: nil,
@@ -124,41 +169,64 @@ defmodule XUnitFormatter.Error do
             skipped: nil,
             failure: %XUnitFormatter.Failure{}
 
-  defp map_key_to_xml_key({k, v}) when is_atom(k), do: map_key_to_xml_key({to_string(k), v})
-  defp map_key_to_xml_key({k, v}) when is_binary(k), do: {String.replace(k, "_", "-"), v}
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate xunit_xml(data), to: XUnitFormatter.XUnitXML.Any
 
-  def to_xml(error = %__MODULE__{}) do
-    attrs = error |> Map.from_struct() |> Map.drop(@child_elements) |> Enum.reject(&is_nil/1) |> Enum.map(&map_key_to_xml_key/1) |> Enum.into(%{})
-    element(:error, attrs, [])
+    def element_name(_), do: :error
+    def content(data), do: [XUnitFormatter.XUnitXML.xunit_xml(data.failure)]
+    def child_elements(_), do: [:failure]
   end
+
 end
 
 defmodule XUnitFormatter.Test do
-  @child_elements [:traits, :failure, :reason]
   @enforce_keys [:name]
   defstruct name: nil,
             type: nil,
             method: nil,
             time: nil,
             result: nil,
-            traits: nil,
-            failure: %XUnitFormatter.Failure{},
-            reason: ""
+            traits: [],
+            failure: nil,
+            reason: nil
 
-  defp map_key_to_xml_key({k, v}) when is_atom(k), do: map_key_to_xml_key({to_string(k), v})
-  defp map_key_to_xml_key({k, v}) when is_binary(k), do: {String.replace(k, "_", "-"), v}
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate xunit_xml(data), to: XUnitFormatter.XUnitXML.Any
 
-  def to_xml(test = %__MODULE__{}) do
-    attrs = test |> Map.from_struct() |> Map.drop(@child_elements) |> Enum.reject(&is_nil/1) |> Enum.map(&map_key_to_xml_key/1) |> Enum.into(%{})
-    {:test, attrs, []}
+    def element_name(_), do: :test
+    def content(data) do
+      if is_list(data.traits) and length(data.traits) > 1 do
+        [traits: (data.traits |> Enum.map(&XUnitFormatter.XUnitXML.xunit_xml/1))]
+      else
+        []
+      end
+      ++
+      if is_nil(data.failure) do
+        []
+      else
+        [failure: (data.failure |> XUnitFormatter.XUnitXML.xunit_xml())]
+      end
+      ++
+      if is_nil(data.reason) do
+        []
+      else
+        [reason: data.reason]
+      end
+    end
+    def child_elements(_), do: [:traits, :failure, :reason]
   end
 end
 
 defmodule XUnitFormatter.Trait do
   defstruct [:name, :value]
+  defimpl XUnitFormatter.XUnitXML do
+    defdelegate attributes(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate content(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate child_elements(data), to: XUnitFormatter.XUnitXML.Any
+    defdelegate xunit_xml(data), to: XUnitFormatter.XUnitXML.Any
 
-  def to_xml(trait = %__MODULE__{}) do
-    attrs = trait |> Map.from_struct()
-    {:trait, attrs, []}
+    def element_name(_), do: :trait
   end
 end
--- test/xunit_formatter_test.exs
+++ test/xunit_formatter_test.exs
@@ -1,4 +1,32 @@
-defmodule XUnitFormatterTest do
+defmodule XUnitFormatterDocTest do
   use ExUnit.Case
   doctest XUnitFormatter
+end
+
+defmodule XUnitFormatterStructuresRootDocumentTest do
+  use ExUnit.Case
+
+  test "Root document contains proper XML when empty" do
+    %XUnitFormatter.Document{} |> XUnitFormatter.XUnitXML.xunit_xml() |> IO.inspect()
+  end
+
+  test "Empty assembly is still proper XML" do
+    %XUnitFormatter.Document{assemblies: [%XUnitFormatter.Assembly{name: "my_test_assembly", total: 0, collections: []}]} |> XUnitFormatter.XUnitXML.xunit_xml() |> IO.inspect()
+  end
+
+  test "Empty collections is still proper XML" do
+    %XUnitFormatter.Document{
+      assemblies: [
+        %XUnitFormatter.Assembly{name: "my_test_assembly1", total: 1, collections: [
+          %XUnitFormatter.Collection{name: "TestCaseCollection1"}
+        ]},
+        %XUnitFormatter.Assembly{name: "my_test_assembly2", total: 2, collections: [
+          %XUnitFormatter.Collection{name: "TestCaseCollection2"},
+          %XUnitFormatter.Collection{name: "TestCaseCollection3"}
+        ]}
+      ]
+    }
+    |> XUnitFormatter.XUnitXML.xunit_xml()
+    |> IO.inspect()
+  end
 end
